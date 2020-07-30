30bb97040adb346b5ab60c433def50425aa51cd4
pratikwaware <pratikwaware919@gmail.com>
2020-07-30 15:06:12 +0530

    Updates

---
+++ precache-manifest.717d7b5eba79c2733588b1de125cbaa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0e0645214c6a4948905da195a5e59d",
+    "url": "/mobile-ecommerce/index.html"
+  },
+  {
+    "revision": "520d27898c480d4ee4ca",
+    "url": "/mobile-ecommerce/static/css/main.6126f80f.chunk.css"
+  },
+  {
+    "revision": "1b4f270695e7ab4626d3",
+    "url": "/mobile-ecommerce/static/js/2.c8fe8d1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mobile-ecommerce/static/js/2.c8fe8d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520d27898c480d4ee4ca",
+    "url": "/mobile-ecommerce/static/js/main.88ba6fc7.chunk.js"
+  },
+  {
+    "revision": "977f058d3b8e993fd914",
+    "url": "/mobile-ecommerce/static/js/runtime-main.3850e7b9.js"
+  }
+]);
